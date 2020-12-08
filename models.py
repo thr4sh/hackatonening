e42000080c16c9c22e8907f586e6bd83973f41e4
thr4sh <rectal0smasher@gmail.com>
2020-12-08 13:17:50 +0700

    Added schemas for models

--- models.py
+++ models.py
@@ -80,17 +80,23 @@ class Teacher(db.Model):
     __tablename__ = 'teacher'
     id = db.Column('ID', db.String, primary_key = True, nullable = False)
     name = db.Column('NAME', db.String(200), nullable = False)
+    children = db.relationship(
+        "Schedule",
+        secondary="TeacherSchedule",
+        back_populates="Teacher")
 
-class Teacher(db.Model):
+class TeacherSchedule(db.Model):
+    __tablename__ = 'teacher_schedule'
+    id_teacher = db.Column('ID_TEACHER', db.String, db.ForeignKey('lesson.id'), nullable=False)
+    id_schedule = db.Column('ID_SCHEDULE', db.String, db.ForeignKey('lesson.id'), nullable=False)
+
+class Time(db.Model):
     __tablename__ = 'time'
     id = db.Column('ID', db.String, primary_key = True, nullable = False)
     beg = db.Column('BEG', db.DateTime, nullable = False)
     end = db.Column('END', db.DateTime, nullable = False)
 
-    children = db.relationship(
-        "Schedule",
-        secondary="TeacherSchedule",
-        back_populates="Teacher")
+
 
 class Schedule(db.Model):
     __tablename__ = 'schedule'
@@ -107,76 +113,44 @@ class Schedule(db.Model):
         secondary="TeacherSchedule",
         back_populates="Schedule")
 
-class TeacherSchedule(db.Model):
-    __tablename__ = 'teacher_schedule'
-    id_teacher = db.Column('ID_TEACHER', db.String, db.ForeignKey('lesson.id'), nullable=False)
-    id_schedule = db.Column('ID_SCHEDULE', db.String, db.ForeignKey('lesson.id'), nullable=False)
+
 
 #schemas start here
-
-class Person(db.Model):
-    __tablename__ = 'person'
-    person_id = db.Column(db.Integer, primary_key=True)
-    lname = db.Column(db.String(32))
-    fname = db.Column(db.String(32))
-    timestamp = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
-    )
-    notes = db.relationship(
-        'Note',
-        backref='person',
-        cascade='all, delete, delete-orphan',
-        single_parent=True,
-        order_by='desc(Note.timestamp)'
-    )
-
-
-class Note(db.Model):
-    __tablename__ = 'note'
-    note_id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.Integer, db.ForeignKey('person.person_id'))
-    content = db.Column(db.String, nullable=False)
-    timestamp = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
-    )
-
-#ModelSchema --> SQLAlchemyAutoSchema in new versions
-class PersonSchema(ma.SQLAlchemyAutoSchema):
+class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Person
-        sqla_session = db.session
-        include_relationships = True
-        load_instance = True
-    notes = Nested('PersonNoteSchema', default=[], many=True)
-
-class PersonNoteSchema(ma.SQLAlchemyAutoSchema):
-    """
-    This class exists to get around a recursion issue
-    """
-    note_id = fields.Int()
-    person_id = fields.Int()
-    content = fields.Str()
-    timestamp = fields.Str()
-
-class NoteSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Note
+        model = User
         sqla_session = db.session
         include_relationships = True
         load_instance = True
 
-    note_id = fields.Int()
-    person_id = fields.Int()
-    content = fields.Str()
-    timestamp = fields.Str()
+#???
+class FacultySchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Faculty
+        sqla_session = db.session
+        include_relationships = True
+        load_instance = True
 
-    person = Nested('NotePersonSchema', default=None)
+class TeacherSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Teacher
+        sqla_session = db.session
+        include_relationships = False
+        load_instance = True
 
-class NotePersonSchema(ma.SQLAlchemyAutoSchema):
-    """
-    Этот класс существует, чтобы обойти проблему рекурсии
-    """
-    person_id = fields.Int()
-    lname = fields.Str()
-    fname = fields.Str()
-    timestamp = fields.Str()
+class AuditorySchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Auditory
+        sqla_session = db.session
+        include_relationships = False
+        load_instance = True
+
+class ScheduleSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Auditory
+        sqla_session = db.session
+        include_relationships = False
+        load_instance = True
+    teachers = Nested('TeacherSchema', default= [], many = True)
+
+
