b400dc7ae72570c4c4fea793bdd91c68a741477d
thr4sh <rectal0smasher@gmail.com>
2020-12-08 14:16:25 +0700

    Commit

--- valid.py
+++ valid.py
@@ -1,13 +1,22 @@
-from datetime import datetime
+from datetime import date
 from models import User
 import random
 import hashlib
-from sqlalchemy import text
+
 
 def gen_token():
     token = hashlib.md5(bytes(int(random * 10000000.0))).hexdigest()
     return token
 
-def valid(id, token):
-    res = User.query.filter(User.id == id).filter(User.token == token).filter(text(f'EXPIRATION<{datetime}'))
+
+def valid(tid, token):
+    res = User.query.filter(User.id == tid).filter(User.token == token)
+    if res is not None:
+        print(res)
+    #if res is not None:
+    #    if date(res.expiration) < date.today():
+    #        return True
+
     return res is not None
+
+
