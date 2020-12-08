883c7ac8c38347bd2f248ab1a17c215cd3f5dc36
thr4sh <rectal0smasher@gmail.com>
2020-12-08 17:15:21 +0700

    P3P p#p

--- main.py
+++ main.py
@@ -3,22 +3,17 @@ import connexion
 import random
 from config import connex_app, app
 from valid import gen_token
-# Создадим экземпляр приложения
+
 #people're telling that two instances causes stupid crashes
 #app = connexion.App(__name__, specification_dir='./')
 
-# Прочитаем файл swagger.yml для настройки конечных точек
+
 connex_app.add_api('swagger2.yml')
 
 random.seed()
-# Создадим маршрут URL в нашем приложении для "/"
+
 @connex_app.route('/')
 def home():
-    """
-    Эта функция просто отвечает на URL "localhost:5000/" в браузера
-
-    :return:        подствляет шаблон 'home.html'
-    """
     return render_template('home.html')
 
 
